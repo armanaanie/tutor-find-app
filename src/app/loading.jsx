fe40a0d632583e301c7d1d2b536810adbd3ffcb5
Armana Anie <viviandesena96@gmail.com>
2026-05-22 21:44:34 +0600

    loading jsx add

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -7,7 +7,7 @@ export default function Loading() {
         <div className="absolute inset-0 rounded-full border-4 border-blue-500 border-t-transparent animate-spin"></div>
       </div>
 
-      <p className="ml-4 text-gray-600 font-medium">Loading...</p>
+      <p className="ml-4 text-gray-600 font-medium">Loading....</p>
     </div>
   );
 }
